7c9c9ce1bbfb3faaa871182237a5047abbc36697
pbibulic <petar@bitwerk.io>
2023-07-05 20:21:11 +0200

    Rename /firebase/auth to evade name colisions. Add input fields label float

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -10,12 +10,27 @@ const LoginForm = (props: Props) => {
     <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
       <form>
         <div className="relative">
-          <input type="text" placeholder="Username" className="input input-bordered w-full mb-4" />
-          <label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
-            Floating outlined
+          <input type="text" placeholder=" " className="input input-bordered w-full mb-4 peer z-10" />
+          <label
+            className={`absolute rounded-2xl text-gray-500 dark:text-gray-400 duration-300 pointer-events-none transform -translate-y-5
+            scale-75 top-2 origin-[0] bg-base-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 
+            peer-placeholder-shown:scale-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 
+            peer-focus:scale-75 left-1`}
+          >
+            Username
           </label>
         </div>
-        <input type="password" placeholder="Password" className="input input-bordered  w-full mb-4" />
+        <div className="relative">
+          <input type="password" placeholder=" " className="input input-bordered w-full mb-4 peer z-10" />
+          <label
+            className={`absolute rounded-2xl text-gray-500 dark:text-gray-400 duration-300 pointer-events-none transform -translate-y-5
+              scale-75 top-2 origin-[0] bg-base-100 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 
+              peer-placeholder-shown:scale-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 
+              peer-focus:scale-75 left-1`}
+          >
+            Password
+          </label>
+        </div>
         <div className="mb-6 flex items-center justify-between">
           <div className="form-control">
             <label className="cursor-pointer label">
